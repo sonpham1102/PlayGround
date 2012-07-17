3bd06d76250040c487bc266d00f9afc318de0fcb
Vorkaaz <jparlour@rogers.com>
2012-07-17 14:54:47 -0400

    Initial Commit

---
+++ PlayGroound/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PlayGroound
+//
+//  Created by Jason Parlour on 12-07-17.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}

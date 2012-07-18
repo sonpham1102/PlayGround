799d01c69d93e8d9a510f959c0f377a2011d1e16
Alex Parlour <aparlour@hotmail.com>
2012-07-18 13:47:58 -0400

    changed Gamemanager to .mm to allow include of HelloWorldLayer (it includes some C++ files)

---
+++ PlayGroound/GameManager.mm
@@ -0,0 +1,112 @@
+//
+//  GameManager.m
+//  PlayGroound
+//
+//  Created by alex on 12-07-18.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameManager.h"
+#import "cocos2d.h"
+#import "MainMenuScene.h"
+#import "IntroLayer.h"
+#import "HelloWorldLayer.h"
+
+
+@implementation GameManager
+
+//the shared gamemanager variable, returned anytime anyone calls [GameManager sharedGameManager]
+//static and starts as nil
+static GameManager* _sharedGameManager = nil;
+
+//creates the _sharedGameManager (if necessary) and returns a pointer to it
++(GameManager*) sharedGameManager
+{
+    // the @synchronize directive is used to threadlock this code I think, to prevent multiple threads from trying
+    // to create the singleton
+    @synchronized ([GameManager class])
+    {
+        if (!_sharedGameManager)
+        {
+            [[self alloc] init];
+        }
+        return _sharedGameManager;
+    }
+}
+
++(id) alloc
+{
+    @synchronized ([GameManager class])
+    {
+        NSAssert(_sharedGameManager == nil, @"Trying to instantiate a second game manager");
+        _sharedGameManager = [super alloc];
+        return _sharedGameManager;
+    }
+    // AP: not sure how this line would ever get called, took it from lcc2d example
+    return nil;
+}
+
+-(id) init
+{
+    self = [super init];
+    if (self != nil)
+    {
+        CCLOG(@"Game Manager initialized");
+        currentLevel = kLevelUnitialized;
+    }
+    return self;
+}
+
+-(void) runLevelWithID:(LevelIDs)levelID
+{
+    LevelIDs oldLevel = currentLevel;
+    currentLevel = levelID;
+    id sceneToRun = nil;
+    
+    switch (levelID) {
+        case kMainMenu:
+            sceneToRun = [MainMenuScene node];
+            break;
+        case kPlayGround1:
+            sceneToRun = [IntroLayer scene];
+            break;
+        case kPlayGround2:
+            sceneToRun = [HelloWorldLayer scene];
+            break;
+            
+        default:
+            CCLOG(@"Unknown Level ID, can't change scenes");
+            return;
+            break;
+    }
+    
+    //see if a new scene was found
+    if (sceneToRun == nil)
+    {
+        //nope, so nothing to do
+        currentLevel = oldLevel;
+        return;
+    }
+    
+    //Special case Menu code goes here
+    if (IS_IPAD())
+    {
+        
+    }
+    else 
+    {
+        
+    }
+    
+    //switch to the new Level's scene
+    if ([[CCDirector sharedDirector] runningScene] == nil)
+    {
+        [[CCDirector sharedDirector] runWithScene:sceneToRun];
+   }
+    else
+    {
+        [[CCDirector sharedDirector] replaceScene:sceneToRun];
+    }
+}
+
+@end
